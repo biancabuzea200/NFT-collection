2f8bb643910b52b93472d07c615d0d8dc1e7d84f
biancabuzea200 <bianca@lukso.io>
2024-04-23 16:53:38 +0200

    add scripts

---
+++ scripts/generateMetadataJSON.ts
@@ -0,0 +1,86 @@
+import { existsSync, readFileSync, writeFileSync } from "fs";
+import { ethers } from "ethers";
+
+const main = () => {
+  if (existsSync("assets/metadata.json")) {
+    console.log("`metadata.json` already exists in the assets folder.");
+    return;
+  }
+
+  const firstImage = readFileSync("assets/Butter.png").toString("hex");
+  const secondImage = readFileSync("assets/Marsh.png").toString("hex");
+  const thirdImage = readFileSync("assets/Sprinkle.png").toString("hex");
+  const iconImage = readFileSync("assets/icon.png").toString("hex");
+
+  const name = "Your collectible name";
+  const description = "Description for your collectible";
+  const links = [{ title: "Twitter", url: "https://twitter.com/" }];
+  const attributes = [
+    { key: "Unrevealed", value: true, type: "boolean" },
+    { key: "Background", value: "Purple", type: "string" },
+    { key: "Age", value: 10, type: "number" },
+  ];
+  const images = [
+    [
+      {
+        width: 1024,
+        height: 1024,
+        url: "ipfs://QmVfYb9D6x5hNuAaMa1qmoS2LmGi9Z78B1JKKaMyedv7wv/Butter.png",
+        verification: {
+          method: "keccak256(bytes)",
+          hash: ethers.keccak256(`0x${firstImage}`),
+        },
+      },
+    ],
+    [
+      {
+        width: 1024,
+        height: 1024,
+        url: "ipfs://Qmf4r4LcBG6XsRzQDTjEjNcxcLhUt4xyTt3aZiFgbBwUZo/Marsh.png",
+        verification: {
+          method: "keccak256(bytes)",
+          hash: ethers.keccak256(`0x${secondImage}`),
+        },
+      },
+    ],
+    [
+      {
+        width: 1024,
+        height: 1024,
+        url: "ipfs://QmZR5P4o322gTtAxsbt2gm93e7bYqBhnjaMog4HnjYmAFt/Sprinkle.png",
+        verification: {
+          method: "keccak256(bytes)",
+          hash: ethers.keccak256(`0x${thirdImage}`),
+        },
+      },
+    ],
+  ];
+  
+  const icon = [
+    {
+      width: 1024,
+      height: 1024,
+      url: "ipfs://QmPS3n3xe5gQsGpUnpaYWvzug44JknBZbUv6cv8yKJ7Gsi/icon.png",
+      verification: {
+        method: "keccak256(bytes)",
+        hash: ethers.keccak256(`0x${iconImage}`),
+      },
+    },
+  ];
+
+  const json = {
+    LSP4Metadata: {
+      name,
+      description,
+      links,
+      attributes,
+      images,
+      icon,
+      assets: [],
+    },
+  };
+
+  writeFileSync("assets/metadata.json", JSON.stringify(json));
+};
+
+main();
